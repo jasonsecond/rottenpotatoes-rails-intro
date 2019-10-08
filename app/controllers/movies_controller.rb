eae6578e594fb5aaf4042239c59d1bbf5e547167
jasonlien0312 <ec2-user@ip-172-31-28-7.us-east-2.compute.internal>
2019-10-08 15:12:29 +0000

    updated controller, application.haml, and index.haml

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,9 @@
 class MoviesController < ApplicationController
-
+  
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
+    session[:fliter] = nil
+    session[:order] = nil
   end
 
   def show
@@ -9,13 +11,62 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
+  
+  helper_method :bg_color_class
+  def bg_color_class(str)
+    if params[:order] == str
+      'hilite'
+    end
+  end
+  helper_method :rating_help
+  def rating_help(rating)
+    if params["ratings"] == nil
+      if !params[:submitted] #just entered page
+        true
+      else ################################################use intentionally unchecked everything
+        false
+      end
+    else# something was checked
+      params["ratings"].has_key?(rating)
+    end 
+  end
 
   def index
-    @movies = Movie.all
+    #setting up variables
+    @all_ratings = Movie.get_raitings
+    if params.key?(:order)
+      session[:order] = params[:order]
+    elsif session.key?(:order)
+      params[:order] = session[:order]
+      redirect_to movies_path(params) and return
+    end
+    
+    # @slected_ratings = params.key?("ratings") ? params["ratings"].keys : @all_ratings
+    
+    if params.key?("ratings")
+      session[:filter] = params["ratings"]
+      @slected_ratings = params["ratings"].keys
+    else
+      if params[:utf8]
+        @selected_ratings = []
+      elsif session.key?(:filter)
+        params["ratings"] = session[:filter]
+        redirect_to movies_path(params) and return
+      else
+        @selected_ratings = @all_ratings
+      end
+    end
+    
+    
+    @movies = Movie.order(params[:order]).where(:rating => @slected_ratings)
+      
+    #rails s -p $PORT -b $IP
   end
 
   def new
     # default: render 'new' template
+    session[:fliter] = nil
+    session[:order] = nil
   end
 
   def create
@@ -40,6 +91,7 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+    # session[:fliter] = nil
+    # session[:order] = nil
   end
-
 end
